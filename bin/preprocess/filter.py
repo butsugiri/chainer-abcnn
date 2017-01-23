75f45d1db5790fd74e2ba69ea470e064885fd1aa
Shun Kiyono <kiyono@ecei.tohoku.ac.jp>
2017-01-23 12:38:31 +0900

    データ整理，前処理スクリプト追加

---
+++ bin/preprocess/filter.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+import sys
+import argparse
+from itertools import groupby
+
+def main(fi, data_type):
+    filter_doc_path = "../../data/WikiQACorpus/WikiQA-{}-filtered.ref".format(data_type)
+    targets = {x.split()[0]:1 for x in open(filter_doc_path)}
+
+    for line in fi:
+        line = line.strip()
+        question_id = line.split()[0]
+        if question_id in targets:
+            print(line)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="hogehoge")
+    parser.add_argument('--data', default='train', type=str, help='data type')
+    args = parser.parse_args()
+    main(sys.stdin, args.data)
---
+++ bin/preprocess/jsonify.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+WikiQAの中身をtokenizeしたものに置換するスクリプト
+1行1jsonで吐きます
+"""
+import sys
+import argparse
+import json
+from itertools import groupby
+
+def main(fi):
+    for line in fi:
+        data = line.strip().split("\t")
+        question_id = data[1]
+        document_id = data[3]
+        sentence_id = data[5]
+        title = data[4]
+        question = data[-3]
+        answer = data[-2]
+        label = data[-1]
+
+        out = {
+            'question_id': question_id,
+            'document_id': document_id,
+            'sentence_id': sentence_id,
+            'title': title,
+            'question': question,
+            'answer': answer,
+            'label': label
+        }
+
+        print(json.dumps(out))
+
+
+
+if __name__ == "__main__":
+    main(sys.stdin)
